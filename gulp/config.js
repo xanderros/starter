a8cbedf73f3cc53886a5adefda2bb2b459ed8fc9
aleksandr <alex@websecret.by>
2020-10-23 13:56:33 +0300

    Full markup

--- gulp/config.js
+++ gulp/config.js
@@ -1,65 +1,35 @@
-var util = require('gulp-util');
-
-var production = util.env.production || util.env.prod || false;
-var destPath = 'build';
-
-var config = {
-    env       : 'development',
-    production: production,
-
-    src: {
-        root         : 'src',
-        templates    : 'src/templates',
-        templatesData: 'src/templates/data',
-        pagelist     : 'src/index.yaml',
-        sass         : 'src/sass',
-        // path for sass files that will be generated automatically via some of tasks
-        sassGen      : 'src/sass/generated',
-        img          : 'src/img',
-        svg          : 'src/img/svg',
-        icons        : 'src/icons',
-        // path to png sources for sprite:png task
-        iconsPng     : 'src/icons',
-        // path to svg sources for sprite:svg task
-        iconsSvg     : 'src/icons',
-        // path to svg sources for iconfont task
-        iconsFont    : 'src/icons',
-        fonts        : 'src/fonts',
-        lib          : 'src/lib',
-		js           : 'src/js',
-        jsAll        : [
-        				'./node_modules/jquery/dist/jquery.min.js',
-        				'./node_modules/slick-carousel/slick/slick.min.js',
-            			'src/js/*.js'
-		]
-    },
-    dest: {
-        root : destPath,
-        html : destPath,
-        css  : destPath + '/css',
-        js   : destPath + '/js',
-        img  : destPath + '/img',
-        fonts: destPath + '/fonts',
-        lib  : destPath + '/lib'
-    },
-
-    setEnv: function(env) {
-        if (typeof env !== 'string') return;
-        this.env = env;
-        this.production = env === 'production';
-        process.env.NODE_ENV = env;
-    },
-
-    logEnv: function() {
-        util.log(
-            'Environment:',
-            util.colors.white.bgRed(' ' + process.env.NODE_ENV + ' ')
-        );
-    },
-
-    errorHandler: require('./util/handle-errors')
-};
-
-config.setEnv(production ? 'production' : 'development');
-
-module.exports = config;
+export const paths = {
+	styles: {
+		src: ['src/sass/_main.scss', 'src/sass/helpers/_variables.scss', 'src/sass/styles.scss'],
+		dest: 'build/css/'
+	},
+	scripts: { // Scripts for the whole site
+		src: [
+      './node_modules/jquery/dist/jquery.min.js',
+      './node_modules/lazysizes/lazysizes.min.js',
+      './node_modules/gsap/dist/gsap.min.js',
+      './node_modules/scrollmagic/scrollmagic/minified/ScrollMagic.min.js',
+      './node_modules/scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js',
+      './node_modules/smooth-scroll/dist/smooth-scroll.polyfills.min.js',
+      './node_modules/scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js',//
+			'src/js/main.js'
+		],
+		dest: 'build/js/'
+	},
+	views: {
+		src: 'src/templates/**/[^_]*.html',
+		dest: 'build/'
+	},
+	php: {
+		src: 'src/php/*.php',
+		dest: 'build/'
+	},
+	fonts: {
+		src: 'src/fonts/**/*',
+		dest: 'build/fonts'
+	},
+	images: {
+		src: 'src/img/**/*',
+		dest: 'build/img/'
+	}
+}
---
+++ src/js/main.js
@@ -0,0 +1,514 @@
+$(document).ready(function () {
+
+  /*** Mobile navigation open/close ***/
+  $(document).on('click', '.nav__btn', navToggle);
+
+  function navToggle(e) {
+    e.preventDefault();
+
+    var $body = $('body');
+
+    if ($body.hasClass('nav-opened')) {
+      $body.removeClass('nav-opened');
+    }
+    else {
+      $body.addClass('nav-opened');
+    }
+  }
+
+  // close mobile navigation on anchor click
+  $(document).on('click', '.nav__link.anchor[href*="#"]', closeMobNav);
+
+  function closeMobNav(e) {
+    if ($("body").hasClass('nav-opened')) {
+      $("body").removeClass('nav-opened');
+    }
+  }
+
+  /*** Anchor links smooth scroll ***/
+  var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 600,
+    speedAsDuration: false,
+    durationMin: 500,
+    topOnEmptyHash: false,
+    easing: 'easeInOutCubic'
+  });
+
+  /*** Popup ***/
+  var popup = document.querySelectorAll(".popup");
+  var popupShowBtn = document.querySelectorAll("*[data-popup]");
+  var popupCloseBtn = document.querySelectorAll(".popup__close");
+
+  function showPopup(e) {
+    e.preventDefault();
+
+    var id = this.getAttribute('data-popup');
+    var popupCurrent = document.getElementById(id);
+
+    popupCurrent.classList.add("popup_show");
+    document.querySelector("body").classList.add("overflow-hidden");
+
+    var scriptData = document.querySelectorAll("script[data-src]");
+
+    if ( scriptData ) {
+      for (var i=0; i<scriptData.length; i++) {
+        if(scriptData[i].getAttribute('data-src')) {
+          scriptData[i].setAttribute('src',scriptData[i].getAttribute('data-src'));
+          scriptData[i].removeAttribute('data-src');
+        }
+      }
+    }
+  }
+
+  function closePopup(e) {
+    e.preventDefault();
+
+    this.closest('.popup').classList.remove("popup_show");
+    document.querySelector("body").classList.remove("overflow-hidden");
+  }
+
+  function windowOnClick(event) {
+    if (event.target === this) {
+      this.classList.remove("popup_show");
+      document.querySelector("body").classList.remove("overflow-hidden");
+    }
+  }
+
+  for (var i = 0; i < popupShowBtn.length; ++i) {
+    popupShowBtn[i].addEventListener('click', showPopup, false);
+  }
+  for (var i = 0; i < popupCloseBtn.length; ++i) {
+    popupCloseBtn[i].addEventListener('click', closePopup, false);
+  }
+  for (var i = 0; i < popup.length; ++i) {
+    popup[i].addEventListener('click', windowOnClick, false);
+  }
+
+  /***********/
+  /****************************** Scroll animations ******************************/
+
+  // init controllers
+  var controllerDefault = new ScrollMagic.Controller();
+  var controllerMain = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: 0}});
+
+  // resize scenes
+  var timeoutDuration = 300;
+  var resizeTimeout = null;
+
+  window.addEventListener('resize', function() {
+    if (resizeTimeout) {
+      clearTimeout(resizeTimeout);
+    }
+    resizeTimeout = setTimeout(function() {
+
+      heroSize();
+
+      sceneH1Opacity.destroy(true);
+      sceneH1Opacity = createSceneH1Opacity();
+
+      scenePink.destroy(true);
+      scenePink = createScenePink();
+
+      scenePhoneBoxY.destroy(true);
+      scenePhoneBoxY = createScenePhoneBoxY();
+
+      scenePhone1X.destroy(true);
+      scenePhone1X = createScenePhone1X();
+
+      scenePhone2X.destroy(true);
+      scenePhone2X = createScenePhone2X();
+
+    }, timeoutDuration);
+  });
+
+  // reload scenes
+  var initialWidth = $(window).width();
+  var initialHeight = $(window).height();
+
+  if ($(".scroll-animations").length) {
+    window.addEventListener('resize', function() {
+
+      var widthNew = $(window).width();
+      var heightNew = $(window).height();
+
+      var indentWidth = widthNew - initialWidth;
+      var indentHeight = heightNew - initialHeight;
+
+      console.log(initialWidth, widthNew);
+
+      if ( indentWidth < -150 || indentWidth > 150 || indentHeight < -150 || indentHeight > 150 ) {
+        window.location.reload();
+      }
+    });
+  }
+
+  /**/
+  /******** Navigation colors ********/
+
+  if ($(".scroll-animations").length) {
+    // Meet section
+    new ScrollMagic.Scene({triggerElement: "#triggerMeet", offset: -30})
+        .setClassToggle(".wrapper", "wrapper_color_meet")
+        .addTo(controllerMain);
+
+    // Developing section
+    new ScrollMagic.Scene({triggerElement: "#triggerDev", offset: -30})
+        .setClassToggle(".wrapper", "wrapper_color_dev")
+        .addTo(controllerMain);
+
+    // Production section
+    new ScrollMagic.Scene({triggerElement: "#triggerProd", offset: -30})
+        .setClassToggle(".wrapper", "wrapper_color_prod")
+        .addTo(controllerMain);
+
+    // Cases section
+    new ScrollMagic.Scene({triggerElement: "#triggerCases", offset: -30})
+        .setClassToggle(".wrapper", "wrapper_color_white")
+        .addTo(controllerMain);
+
+    // Contact section
+    new ScrollMagic.Scene({triggerElement: ".nav-color-black", offset: -30})
+        .setClassToggle(".wrapper", "wrapper_color_black")
+        .addTo(controllerMain);
+  }
+  else {
+    // Black nav color
+    new ScrollMagic.Scene({triggerElement: ".nav-color-black", offset: -30})
+        .setClassToggle(".wrapper", "wrapper_color_black")
+        .addTo(controllerMain);
+  }
+
+
+  /**/
+  /******** Hero ********/
+  function is_mobile() {return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));};
+
+  var hero = $(".hero");
+
+  function heroSize() {
+    var heroHeight = hero.outerHeight();
+
+    if ( is_mobile() ) {
+      hero.css('height', heroHeight);
+    }
+  };
+
+  heroSize();
+
+  // hero Scale
+  var heroScale = gsap.to("#targetHero", 1, {
+    scale: 185,
+    ease: "power3.in",
+    force3D: false
+  });
+
+  new ScrollMagic.Scene({
+    triggerElement: "#triggerMain",
+    triggerHook: 0,
+    duration: "89%",
+    offset: 25
+  })
+      .setTween(heroScale)
+      .addTo(controllerDefault);
+
+  // hero badge Opacity
+  var heroBadgeOpacity = gsap.to("#targetHeroBadge", 1, {
+    opacity: 0,
+    y: 10,
+    ease: "power1.in",
+    force3D: false
+  });
+
+  new ScrollMagic.Scene({
+    triggerElement: "#triggerMain",
+    triggerHook: 0,
+    duration: "16%",
+    offset: 50
+  })
+      .setTween(heroBadgeOpacity)
+      .addTo(controllerDefault);
+
+  // hero Opacity
+  var heroOpacity = gsap.to("#targetHero", 1, {
+    opacity: 0,
+    ease: "power1.in"
+  });
+
+  function createSceneH1Opacity() {
+    return new ScrollMagic.Scene({
+      triggerElement: "#triggerMain",
+      duration: "27.6%",
+      offset: document.documentElement.clientHeight/100*91
+    })
+        .setTween(heroOpacity)
+        .addTo(controllerMain);
+  }
+
+  var sceneH1Opacity = createSceneH1Opacity();
+
+
+  // pink bg
+  function createScenePink() {
+    return new ScrollMagic.Scene({
+      triggerElement: "#triggerMain",
+      duration: "200%",
+      offset: document.documentElement.clientHeight/100*91
+    })
+        .setClassToggle(".container", "active")
+        .addTo(controllerMain);
+  }
+
+  var scenePink = createScenePink();
+
+
+  /**/
+  /******** Process ********/
+
+  // tween Process Opacity
+  var captionOpacity = gsap.to("#targetProcess", 1, {
+    opacity: 1,
+    ease: "power1.in"
+  });
+
+  new ScrollMagic.Scene({
+    triggerElement: "#triggerProcess",
+    triggerHook: 0.5,
+    duration: "55%"
+  })
+      .setTween(captionOpacity)
+      .addTo(controllerDefault);
+
+  /**/
+  /******** Frames ********/
+
+  // all frames
+  var frames = document.querySelectorAll(".frame:not(.frame_cases)");
+  var framesBg = document.querySelectorAll(".frame:not(.frame_cases) .frame__bg");
+
+  // create scene for every slide
+  for (var i=0; i<frames.length; i++) {
+
+    // tweens
+    new ScrollMagic.Scene({triggerElement: frames[i], duration: "50%"})
+        .setTween(gsap.to(framesBg[i], 1, {
+          width: document.documentElement.clientWidth,
+          top: 0,
+          borderRadius: 0,
+          ease: "power1.in"
+        }))
+        .addTo(controllerDefault);
+  }
+
+
+  /**/
+  /******** Slider (horizontal) ********/
+  var sliderContainer = $("#sliderContainer");
+
+  // Slider move
+  var horizontalSlide = gsap.timeline()
+      .to("#sliderContainer", 1,   {
+        x: document.documentElement.clientWidth - sliderContainer.outerWidth(),
+        ease: "sine.inOut"
+      });
+
+  function createSceneSliderMove() {
+    return new ScrollMagic.Scene({
+      triggerElement: "#triggerCases",
+      triggerHook: "onLeave",
+      duration: "200%"
+    })
+        .setTween(horizontalSlide)
+        .addTo(controllerMain);
+  }
+  var sceneSliderMove = createSceneSliderMove();
+
+  // Slider X translate
+  function createSceneSliderX() {
+    return new ScrollMagic.Scene({
+      triggerElement: "#triggerCases",
+      duration: "50%"
+    })
+      .setTween(gsap.to("#sliderBox", 1, {
+        x: 0,
+        ease: "power1.in"
+      }))
+      .addTo(controllerDefault);
+  }
+  var sceneSliderX = createSceneSliderX();
+
+  // Slider frame width
+  function createSceneSliderWidth() {
+    return new ScrollMagic.Scene({
+      triggerElement: "#triggerCases",
+      duration: "50%"
+    })
+      .setTween(gsap.to(".frame_cases .frame__slider", 1, {
+        width: document.documentElement.clientWidth,
+        ease: "power1.in"
+      }))
+      .addTo(controllerDefault);
+  }
+  var sceneSliderWidth = createSceneSliderWidth();
+
+  // Slider frame style
+  function createSceneSliderStyle() {
+    return new ScrollMagic.Scene({
+      triggerElement: "#triggerCases",
+      duration: "50%"
+    })
+      .setTween(gsap.to(".frame_cases .frame__bg", .3, {
+        width: document.documentElement.clientWidth,
+        top: 0,
+        borderRadius: 0,
+        ease: "power1.in"
+      }))
+      .addTo(controllerDefault);
+  }
+  var sceneSliderStyle = createSceneSliderStyle();
+
+
+
+  /**/
+  /******** Phone ********/
+  // tween Phone Box Y
+  var phoneY = gsap.to("#targetPhoneBox", 1, {
+        y: 0,
+        ease: "power1.out",
+        force3D: false
+  });
+
+  function createScenePhoneBoxY() {
+    return new ScrollMagic.Scene({
+      triggerElement: "#triggerPhone",
+      triggerHook: 0.6,
+      duration: "25%"
+    })
+        .setTween(phoneY)
+        // .addIndicators({name: "Y phone", colorStart: "#f7f", colorEnd: "#f7f"})
+        .addTo(controllerDefault);
+  }
+
+  var scenePhoneBoxY = createScenePhoneBoxY();
+
+  // tween Phone 1 X
+  var phone1X = gsap.to("#targetPhone1", 1, {
+        x: 0,
+        ease: "power1.in",
+        force3D: false
+      });
+
+  function createScenePhone1X() {
+    return new ScrollMagic.Scene({
+      triggerElement: "#triggerPhone",
+      triggerHook: 0.35,
+      duration: "30%"
+    })
+        .setTween(phone1X)
+        // .addIndicators({name: "left phone", colorStart: "#900", colorEnd: "#900"})
+        .addTo(controllerDefault);
+  }
+
+  var scenePhone1X = createScenePhone1X();
+
+  // tween Phone 2 X
+  var phone2X = gsap.to("#targetPhone2", 1, {
+        x: 0,
+        ease: "power1.in",
+        force3D: false
+      });
+
+  function createScenePhone2X() {
+    return new ScrollMagic.Scene({
+      triggerElement: "#triggerPhone",
+      triggerHook: 0.35,
+      duration: "30%"
+    })
+        .setTween(phone2X)
+        .addTo(controllerDefault);
+  }
+
+  var scenePhone2X = createScenePhone2X();
+
+
+
+
+  /**/
+  /******** Parallax 1 ********/
+  var parallaxTop = gsap.to(".parallax-top", 1, {
+    y: -70,
+    ease: "power1.out"
+  });
+
+  function createParallaxTop() {
+    return new ScrollMagic.Scene({
+      triggerElement: "#triggerParallaxTop",
+      triggerHook: 0,
+      duration: "100%"
+    })
+        .setTween(parallaxTop)
+        .addTo(controllerDefault);
+  }
+
+  var sceneParallaxTop = createParallaxTop();
+
+  /* parallax default */
+  var parallax = document.querySelectorAll(".parallax");
+
+  // create scene for every slide
+  for (var i=0; i<parallax.length; i++) {
+
+    // tweens
+    new ScrollMagic.Scene({
+      triggerElement: parallax[i],
+      triggerHook: 1,
+      duration: "150%"
+    })
+        .setTween(gsap.to(parallax[i], 1, {
+          y: 0,
+          force3D: false
+        }))
+        .addTo(controllerDefault);
+  }
+
+  /*** Forms validate and submit ***/
+
+  /* Form Contact */
+  $("#form-contact").submit(function(e) {
+    var $form = $(this);
+
+    if (this.checkValidity()) {
+      $form.find('button[type=submit]').prop('disabled', true);
+      var data = new FormData(this);
+      $.ajax({
+        url: '/form-contact.php',
+        method: 'POST',
+        data: data,
+        contentType: false,
+        processData: false,
+        complete: function () {
+          $form.find('button[type=submit]').prop('disabled', false);
+        },
+        success: function (data) {
+          if (data != 'OK') {
+            alert(data);
+            return;
+          }
+          // success message
+          $form.addClass('form_sended');
+
+          // scroll to top of form
+          var formY = $form.offset().top;
+          var scrolled = $(window).scrollTop();
+
+          if (scrolled > formY) {
+            $("html, body").animate({
+              scrollTop: formY - 15
+            }, 600);
+          }
+        }
+      });
+    }
+    e.preventDefault();
+  });
+
+});
